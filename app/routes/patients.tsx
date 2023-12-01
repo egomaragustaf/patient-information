7ed72a5f66d6ffe8a34631e8ac2b38eb1f59795e
Ego Maragustaf <egomaragustaf@gmail.com>
2023-12-01 23:52:36 +0700

    chore: setup prisma
    feat: add data patients
    
    feat: seed data patients

--- app/routes/patients.tsx
+++ app/routes/patients.tsx
@@ -1,9 +1,25 @@
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { Layout } from "~/components/layout/layout";
+import { prisma } from "~/libs/db.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const patients = await prisma.patient.findMany({
+    include: {
+      locations: true,
+    },
+  });
+
+  return json({ patients });
+};
 
 export default function Route() {
+  const { patients } = useLoaderData<typeof loader>();
+
   return (
     <Layout>
       <h1>Patients Page</h1>
+      <pre>{JSON.stringify(patients, null, 2)}</pre>
     </Layout>
   );
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,78 @@
-import { prisma } from "~/libs/db.server"
+import { prisma } from "~/libs/db.server";
+import dataPatients from "./seed-data/patients.json";
 
 async function main() {
-    await prisma.user.create({
-        data: {
-            name: "Ego Maragustaf",
-            email: "egomaragustaf@gmail.com",
-            username: "egomaragustaf",
-        }
-    })    
+  await seedUsers();
+  await seedPatients();
+}
+
+async function seedUsers() {
+  const deletedUsers = await prisma.user.deleteMany();
+  console.info(`🟡 Deleted Users: ${deletedUsers.count} users`);
+
+  const createdUser = await prisma.user.create({
+    data: {
+      firstName: "John",
+      lastName: "Doe",
+      gender: "Male",
+      dateOfBirth: "1980-05-15",
+      contactNumber: "+62 812-3456-7890",
+      email: "john.doe@email.com",
+      locations: {
+        create: {
+          street: "Jl. Utama No. 123",
+          city: "Jakarta",
+          province: "DKI Jakarta",
+          postalCode: "12345",
+          countryCode: "ID",
+        },
+      },
+    },
+  });
+  console.info(`🟢 Created User: ${createdUser} user`);
+}
+
+async function seedPatients() {
+  const deletedPatients = await prisma.patient.deleteMany();
+  console.info(`🟡 Deleted Patients: ${deletedPatients.count} patients`);
+
+  const deletedPatientsLocation = await prisma.patientLocation.deleteMany();
+  console.info(
+    `🟡 Deleted Patients Location: ${deletedPatientsLocation.count} patients`
+  );
+
+  for (const patients of dataPatients) {
+    const newPatients = await prisma.patient.create({
+      data: {
+        firstName: patients.firstName,
+        lastName: patients.lastName,
+        gender: patients.gender,
+        dateOfBirth: patients.dateOfBirth,
+        contactNumber: patients.contactNumber,
+        email: patients.email,
+        locations: {
+          create: {
+            street: patients.locations.street,
+            city: patients.locations.city,
+            province: patients.locations.province,
+            postalCode: patients.locations.postalCode,
+            countryCode: patients.locations.countryCode,
+          },
+        },
+      },
+    });
+    console.info(`🟢 Created Patients: ${newPatients} patients`);
+  }
 }
 
 main()
   .then(async () => {
-    console.info("🔵 Seeding complete")
-    await prisma.$disconnect()
+    console.info("🔵 Seeding complete");
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    console.error("🔴 Seeding failed")
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    console.error("🔴 Seeding failed");
+    await prisma.$disconnect();
+    process.exit(1);
+  });
