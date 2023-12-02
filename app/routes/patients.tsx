e2317a55a35d2eb570a3c92c6f9e7c930057f463
Ego Maragustaf <egomaragustaf@gmail.com>
2023-12-02 12:47:51 +0700

    chore: add table shadcn-ui
    feat: implement table on patients page

--- app/routes/patients.tsx
+++ app/routes/patients.tsx
@@ -1,6 +1,16 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Layout } from "~/components/layout/layout";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
 import { prisma } from "~/libs/db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -8,6 +18,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     include: {
       locations: true,
     },
+    take: 5,
   });
 
   return json({ patients });
@@ -19,7 +30,42 @@ export default function Route() {
   return (
     <Layout>
       <h1>Patients Page</h1>
-      <pre>{JSON.stringify(patients, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(patients, null, 2)}</pre> */}
+
+      <Table>
+        <TableCaption>A list of data patients.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-60">Full Name</TableHead>
+            <TableHead>Gender</TableHead>
+            <TableHead>Date of Birth</TableHead>
+            <TableHead className="text-right">Contact Number</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {patients.map((patient) => {
+            return (
+              <TableRow key={patient.id}>
+                <TableCell className="font-medium">
+                  {patient.firstName} {patient.lastName}
+                </TableCell>
+                <TableCell>{patient.gender}</TableCell>
+                <TableCell>{patient.dateOfBirth}</TableCell>
+                <TableCell className="text-right">
+                  {patient.contactNumber}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell className="text-right" colSpan={4}>
+              Pagination here
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
     </Layout>
   );
 }
