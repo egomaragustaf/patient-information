94f7782ae263f0422493c440f2e9397d5f53b0a2
Ego Maragustaf <egomaragustaf@gmail.com>
2023-12-05 22:19:47 +0700

    chore: add zod for validation
    feat: add schema patient

---
+++ app/schema/patient.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+const id = z.string().min(1, "Existing id is required")
+
+export const schemaPatient = z.object({
+    id,
+    firstName: z.string().min(1, "First name require at least 1 characters").max(50, "First name can't be longer than 50 characters"),
+    lastName: z.string().min(1, "Last name require at least 1 characters").max(50, "Last name can't be longer than 50 characters"),
+    gender: z.string().min(1, "Gender require at least 1 characters").max(15, "Gender can't be longer than 15 characters"),
+    dateOfBirth: z.date(),
+    contactNumber: z.string(),
+    email: z.string().min(1, "Email is required").email("This is not an email"),
+    locations: z.object({
+      street: z.string(),
+      city: z.string(),
+      province: z.string(),
+      countryCode: z.string(),
+      postalCode: z.string()
+    })
+  })
+
+  export const schemaPatientDelete = z.object({
+      id,
+  })
+
+  export const schemaPatientUpdate = z.object({
+    patientId: id,
+    firstName: z.string().min(1, "First name require at least 1 characters").max(50, "First name can't be longer than 50 characters"),
+    lastName: z.string().min(1, "Last name require at least 1 characters").max(50, "Last name can't be longer than 50 characters"),
+    gender: z.string().min(1, "Gender require at least 1 characters").max(15, "Gender can't be longer than 15 characters"),
+    dateOfBirth: z.date(),
+    contactNumber: z.string(),
+    email: z.string().min(1, "Email is required").email("This is not an email"),
+    locations: z.object({
+      street: z.string(),
+      city: z.string(),
+      province: z.string(),
+      countryCode: z.string(),
+      postalCode: z.string()
+    })
+  })
